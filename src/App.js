03f2abc6346c537c1a5c1a481c59752378a391ff
Radu Bortan <bortanradu@gmail.com>
2021-12-14 19:42:47 +0100

    cleaned App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import './App.css';
+import classes from './App.module.css';
 import Navigation from './components/Navigation';
 import Home from './views/Home';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 import Parameters from './views/Parameters';
 import Stocks from './views/Stocks';
 import Ingredients from './views/ingredients/Ingredients';
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-      <Navigation/>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/ingredients" element={<Ingredients/>}/>
-          <Route path="/stocks" element={<Stocks/>}/>
-          <Route path="/parametres" element={<Parameters/>}/>
-        </Routes>
-      </Router>
+    <div className={classes.App}>
+      <Navigation />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/ingredients' element={<Ingredients />} />
+        <Route path='/stocks' element={<Stocks />} />
+        <Route path='/parametres' element={<Parameters />} />
+      </Routes>
     </div>
   );
 }
