bb4590c59c13580aae97c24419262c56e631d70f
Radu Bortan <bortanradu@gmail.com>
2022-01-08 23:04:03 +0100

    fixed database problem

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,14 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from '@firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBs8lRnKg3r90pb-pGbndKFSrT74F0S2QY',
-  authDomain: 'projet-awi-4e549.firebaseapp.com',
-  databaseURL:
-    'https://projet-awi-4e549-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'projet-awi-4e549',
-  storageBucket: 'projet-awi-4e549.appspot.com',
-  messagingSenderId: '199233000129',
-  appId: '1:199233000129:web:942a4a5488965b69b5fa40',
+  apiKey: 'AIzaSyC0SGF6HANvi_cf1GOKfHclnuuSd57kjEY',
+  authDomain: 'temp-project-21d5e.firebaseapp.com',
+  projectId: 'temp-project-21d5e',
+  storageBucket: 'temp-project-21d5e.appspot.com',
+  messagingSenderId: '205229253309',
+  appId: '1:205229253309:web:4c640ae5ec6fc35e9f8dd4',
 };
 
 const app = initializeApp(firebaseConfig);
